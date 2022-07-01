23f4f01eb0dd61352d17e134e94ea2f89f8693f2
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-01 15:00:06 -0700

    Added Completion

--- init.lua
+++ init.lua
@@ -1,15 +1,13 @@
-
-
 require "astatt.keys"
 require "astatt.options"
 require "astatt.plugins"
-require "astatt.colorscheme"
 
+-- Plugins
+require "astatt.colorscheme"
+require "astatt.telescope"
+require "astatt.cmp"
 
 if vim.fn.exists('g:vscode') ~= 0 then
   require "astatt.vscode.settings"
 end
-
-
-
 
--- lua/astatt/plugins.lua
+++ lua/astatt/plugins.lua
@@ -46,8 +46,19 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim"               -- Useful lua functions used ny lots of plugins
   
   use 'nvim-telescope/telescope.nvim'         -- FZF
-  -- Colorschemes
-  use 'eddyekofo94/gruvbox-flat.nvim'
+  use 'eddyekofo94/gruvbox-flat.nvim'       -- Colorscheme
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp"                    -- The completion plugin
+  use "hrsh7th/cmp-buffer"                  -- buffer completions
+  use "hrsh7th/cmp-path"                    -- path completions
+  use "hrsh7th/cmp-cmdline"                 -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip"            -- snippet completions 
+
+  -- snippits
+  use "L3MON4D3/luaSnip"                    -- Snippet engine
+  use "rafamadriz/friendly-snippets"        -- a bunch of snippets to use
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
